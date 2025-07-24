381fc9cf63fac764e8b7dea910164fc723674b74
Akshara <sweetakshara.0715@gmail.com>
2025-07-24 10:09:24 -0700

    Added an actual Navigation page for the second one

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,16 +7,26 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
                 Text("This is the root view 🌳")
-                NavigationLink(destination: Text("You've arrived to the Second View 🎊")) {
-                    Text("Click me!")
+
+                NavigationLink(destination: SecondView()) {
+                    Text("Go to Second View")
+                }
+
+                NavigationLink(destination: Text("You've arrived to the Third View 🎊")) {
+                    Text("Go to Third View")
                 }
             }
             .padding()
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
         }
     }
 }
