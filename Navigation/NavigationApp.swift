7297ca0d5f2f287e9ad13ac754e997369f064cd7
Akshara <sweetakshara.0715@gmail.com>
2025-07-24 09:57:55 -0700

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Tiger on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
